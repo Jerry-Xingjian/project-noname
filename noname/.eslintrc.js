a07c32ba6d4f4c82cd0c9a96cd4be6be210f4bc3
XingjianWang-UPenn <xwang7@seas.upenn.edu>
2022-12-19 02:20:48 -0500

    again

--- noname/.eslintrc.js
+++ noname/.eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "jsx-a11y/label-has-associated-control": [ 2, {
-            "labelComponents": ["label"],
-            "labelAttributes": ["htmlFor"],
-            "controlComponents": ["input"]
-        }],
-        'react/prop-types': ['off']
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/label-has-associated-control': [2, {
+      labelComponents: ['label'],
+      labelAttributes: ['htmlFor'],
+      controlComponents: ['input'],
+    }],
+    'react/prop-types': ['off'],
+  },
+};
