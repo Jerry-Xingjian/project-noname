155208c7ba0c8e76d635e3d3cda19bb6d5deb851
XingjianWang-UPenn <xwang7@seas.upenn.edu>
2022-12-20 16:33:41 -0500

    Update profilesRoute.js

--- noname-backend/routes/profilesRoute.js
+++ noname-backend/routes/profilesRoute.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const profilesController = require('../controllers/profilesController');
 
-router.get('/profile/:id', profilesController.getUserProfileById);
+router.get('/get/profile/:id', profilesController.getUserProfileById);
 router.put('/edit/profile/:id', profilesController.editUserProfileByUserId);
 router.put('/follow', profilesController.updateFollow);
 router.put('/unfollow', profilesController.updateUnfollow);
