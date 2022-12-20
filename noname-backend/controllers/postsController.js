ea80f2e703ada0b04766307083aaf0a2833dc77d
XingjianWang-UPenn <xwang7@seas.upenn.edu>
2022-12-20 02:24:57 -0500

    again

--- noname-backend/controllers/postsController.js
+++ noname-backend/controllers/postsController.js
@@ -20,13 +20,13 @@ const newPost = (async (req, res) => {
   }, JWT_SECRET, { expiresIn: '1h' });
 
   // websocket server url
-  // const url = process.env.NODE_ENV === 'production'
-  //   ? 'https://noname-test-version-1.herokuapp.com'
-  //   : 'ws://localhost:8085/';
+  const url = process.env.NODE_ENV === 'production'
+    ? 'https://noname-test-version-1.herokuapp.com'
+    : 'ws://localhost:8085/';
   // const url = 'ws://localhost:8085/';
 
   // websocket connection with jwt
-  const connection = new WebSocket('ws://localhost:8085/', {
+  const connection = new WebSocket(url, {
     headers: { token: serverToken },
   });
   try {
